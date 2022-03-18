6eb4cbcf060d1257bc6097b596024757c1e98f4d
jiya-f <99090254+jiya-f@users.noreply.github.com>
2022-03-19 02:42:28 +0500

    endurance.fitness
    top-bar/main-menu

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Endurance ~ Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- header-strt -->
+    <header>
+        <!-- top-bar-start -->
+        <div class="top-bar">
+          <a href="#"> <h4> Register Now </h4></a>
+
+        </div>
+        <!-- top-bar-end -->
+
+        <!-- logo & main-menu-strt --> 
+        <div class="main-menu">
+            <div class="logo">
+                <img src="img/ENDURANCE-LOGO.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Trainers</a></li>
+                    <li><a href="#">Plans</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul> 
+            </nav>
+        </div>
+        <!-- logo & main-menu-end --> 
+
+        <!-- section-banner-strt -->
+        <div class="top-banner"></div>
+        <!-- section-banner-end -->
+    </header>
+    <!-- header-end -->
+
+    <!-- Main Section-->
+    <section class="main">
+        <div class="container-grid">
+            <div class="col-8">
+
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+    </section>
+    <!-- Main Section-->
+    <!-- footer -->
+    <footer></footer>
+    <!-- footer -->
+    
+</body>
+</html>
