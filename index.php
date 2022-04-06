63fb783ebfca68bb44fc443e1ea660c16fce8dad
jiya-f <99090254+jiya-f@users.noreply.github.com>
2022-04-06 16:57:41 +0500

    bhand

--- index.php
+++ index.php
@@ -8,37 +8,37 @@
         <div class="container-grid">
             <div class="col-8">
                 <!-- home-intro -->
-        <div class="home-intro">
-            <h4>WHO WE ARE</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui &gt;Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui&gt;Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui&gt;Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis quiexplicabo quia!</p>
-            <a href="#">READ MORE</a>
-        </div>
-        <!-- home-intro -->
-        <!-- our-services -->
-        <div class="our-services">
-            <h4>OUR SERVICES</h4>
-            <div class="service-imgs">
-                <img src="img/our-services/FITNESS-B.jpg" alt="">
-                <img src="img/our-services/WEIGHTS-B.jpg" alt="">
-                <img src="img/our-services/YOGA-B.jpg" alt="">
-                <img src="img/our-services/FITNESS-B.jpg" alt="">
-            </div>
-        </div>
-        <!-- our-services -->
-        <!-- our-trainers -->
-        <div class="t-banner"></div>
-        <!-- our-trainers -->
-        <!-- our-plans -->
-        <div class="our-plans">
-            <div class="plan-img"></div>
-            <div class="plan-content">
-                <h4>SUMMER SPECIAL DISCOUNT</h4>
-                <h3>GET <span>25% OFF</span> WILL OUR NEW PLAN </h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut odio praesentium, fuga assumenda necessitatibus quidem temporibus dolore ab? Suscipit illum repellendus eligendi veritatis nam porro.  </p> 
-                <a href="#">READ MORE</a>
-            </div>
-        </div>
-        <!-- our-plans -->
+                <div class="home-intro">
+                    <h4>WHO WE ARE</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui >Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis quiexplicabo quia!</p>
+                    <a href="#">READ MORE</a>
+                </div>
+                <!-- home-intro -->
+                <!-- our-services -->
+                <div class="our-services">
+                    <h4>OUR SERVICES</h4>
+                    <div class="service-imgs">
+                        <img src="img/our-services/FITNESS-B.jpg" alt="">
+                        <img src="img/our-services/WEIGHTS-B.jpg" alt="">
+                        <img src="img/our-services/YOGA-B.jpg" alt="">
+                        <img src="img/our-services/FITNESS-B.jpg" alt="">
+                    </div>
+                </div>
+                <!-- our-services -->
+                <!-- our-trainers -->
+                <div class="t-banner"></div>
+                <!-- our-trainers -->
+                <!-- our-plans -->
+                <div class="our-plans">
+                    <div class="plan-img"></div>
+                    <div class="plan-content">
+                        <h4>SUMMER SPECIAL DISCOUNT</h4>
+                        <h3>GET <span>25% OFF</span> WILL OUR NEW PLAN </h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut odio praesentium, fuga assumenda necessitatibus quidem temporibus dolore ab? Suscipit illum repellendus eligendi veritatis nam porro.  </p> 
+                        <a href="#">READ MORE</a>
+                    </div>
+                </div>
+                <!-- our-plans -->
             </div>
             <!-- right-sidebar start -->
             <?php include 'includes/right-sidebar.php'?>
