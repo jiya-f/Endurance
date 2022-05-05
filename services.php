8a137740093c309d696f3e858ae2fdd28ec05f2d
jiya-f <99090254+jiya-f@users.noreply.github.com>
2022-05-05 13:01:07 +0500

    no need 5may2022

--- services.php
+++ services.php
@@ -7,6 +7,65 @@
         <!-- section-banner-end -->
         <div class="container-grid">
             <div class="col-8">
+                <!-- home-intro -->
+                <div class="home-intro services">
+                    <h4 class="heading-grey">OUR SERVICES</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui >Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis qui>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt iste unde porro nobis magnam facilis alias, itaque amet iusto ducimus blanditiis quiexplicabo quia!</p>
+                </div>
+                <!-- home-intro -->
+                <!-- who we are strt -->
+                <div class="services-container">
+                    <div class="services-img">
+                        <img src="img/our-services/FITNESS.jpg" alt="">
+                    </div>
+                    <div class="services-content">
+                        <h3>Harley Pasternak: Nutritionist & Trainer <span class="services-heading">FITNESS CLASSES</span> </h3>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. , voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsa excepturi voluptas magni in voluptates inventore, error deleniti quasi natus odit aliquam maxime aspernatur distinctio accusamus harum perferendis. Quod non reprehenderit fuga alias doloremque rerum harum officia error ab, voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <a href="">REGISTER NOW</a>
+                    </div>
+                </div>
+
+                <div class="services-container my-25 reverse">
+                    <div class="services-img">
+                        <img src="img/our-services/YOGA.jpg" alt="">
+                    </div>
+                    <div class="services-content">
+                        <h3>Jillian Michaels: Personal trainer <span class="services-heading">YOGA CLASSES</span> </h3>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. , voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsa excepturi voluptas magni in voluptates inventore, error deleniti quasi natus odit aliquam maxime aspernatur distinctio accusamus harum perferendis. Quod non reprehenderit fuga alias doloremque rerum harum officia error ab, voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <a href="">REGISTER NOW</a>
+                    </div>
+                </div>
+
+                <div class="services-container">
+                    <div class="services-img">
+                        <img src="img/our-services/WEIGHTS.jpg" alt="">
+                    </div>
+                    <div class="services-content">
+                        <h3>Massy Arias: Health Coach & Motivational Speaker <span class="services-heading">WEIGHTS CLASSES</span> </h3>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. , voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsa excepturi voluptas magni in voluptates inventore, error deleniti quasi natus odit aliquam maxime aspernatur distinctio accusamus harum perferendis. Quod non reprehenderit fuga alias doloremque rerum harum officia error ab, voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <a href="">REGISTER NOW</a>
+                    </div>
+                </div>
+
+                <div class="services-container my-25 reverse">
+                    <div class="services-img">
+                        <img src="img/our-services/PILATES.jpg" alt="">
+                    </div>
+                    <div class="services-content">
+                        <h3>Harley : Nutritionist & Trainer <span class="services-heading">PILATES CLASSES</span> </h3>
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. , voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsa excepturi voluptas magni in voluptates inventore, error deleniti quasi natus odit aliquam maxime aspernatur distinctio accusamus harum perferendis. Quod non reprehenderit fuga alias doloremque rerum harum officia error ab, voluptatem magnam inventore quaerat minima? Aliquid!</p>
+                        <a href="">REGISTER NOW</a>
+                    </div>
+                </div>
+                <!-- who we are end -->
             </div>
             <!-- right-sidebar start -->
             <?php include 'includes/right-sidebar.php'?>
